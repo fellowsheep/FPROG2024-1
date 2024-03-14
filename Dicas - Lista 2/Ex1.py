f7062b271488cc38560e34c89e1d8ad6e38e4923
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-03-13 22:13:01 -0300

    Create Ex1.py

---
+++ Dicas - Lista 2/Ex1.py
@@ -0,0 +1,8 @@
+#Ler a quantidade de minutos para converter
+minutos  = float(input('Digite o tempo em minutos: '))
+
+#Transformar em segundos
+segundos = minutos * 60
+
+#Exibir o resultado
+print(minutos, ' minutos equivalem a ', segundos, ' segundos.')
