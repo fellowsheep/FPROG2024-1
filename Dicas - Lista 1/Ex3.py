19203293558c7c2267c40fe66bdd06cd6b53d012
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2024-03-13 21:02:55 -0300

    Adicionado Ex3 e início do Ex4 Lista 1

---
+++ Dicas - Lista 1/Ex3.py
@@ -0,0 +1,16 @@
+# Exibir a mensagem da questão
+print('Qual é o verdadeiro nome do super-herói Batman?')
+print('a) Bruce Wayne')
+print('b) Clark Kent')
+print('c) Peter Parker')
+print('d) Tony Stark')
+print('e) Steve Rogers')
+print()
+# Armazenar a resposta correta em uma variavel
+resposta = 'a'
+
+# Pergunta a resposta do usuário e guardar em uma variável
+respostaUsuario = input('Digite a resposta: ')
+
+# Mensagem informando as respostas
+print('A sua resposta foi ',respostaUsuario,'\nA resposta correta é ',resposta,'.')
---
+++ Dicas - Lista 1/Ex4.py
@@ -0,0 +1,8 @@
+pergunta = 'Qual é o verdadeiro nome do super-herói Batman?'
+alternativaA = 'Bruce Wayne'
+alternativaB = 'Clark Kent'
+
+print(pergunta)
+print('a) ', alternativaA)
+
+#continuar....
